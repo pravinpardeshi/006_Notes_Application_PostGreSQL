bac93f5309ffea1cf658bec8f0034ff123dd1196
Pravin Pardeshi <pravin.pardeshi@gmail.com>
2026-06-15 17:49:08 -0400

    Modified config.py file

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 
-
+# Set postgresql database URL
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql://postgres:postgres@localhost:5432/notes_app",
 )
 
+# Location to store uploaded files
 UPLOAD_DIR = os.getenv("UPLOAD_DIR", "uploads")
 
+# Location for storing the DB backups
 BACKUP_DIR = os.getenv("BACKUP_DIR", "backups")
